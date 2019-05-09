dcc533fbf95b31e4418ae60a6fd79326c78c3c42
Gert Adamson <adamson.gert@hotmail.com>
2019-05-10 00:31:49 +0300

    create project

---
+++ zipzop/src/main/java/zipzop/Main.java
@@ -0,0 +1,16 @@
+package zipzop;
+
+/**
+ *
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world");
+    }
+    
+}
